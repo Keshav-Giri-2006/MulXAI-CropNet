3fb1750848fd55988e8d64f5103e00d1350dd477
Keshav Giri <keshavgiri2006@gmail.com>
2026-07-12 22:59:33 +0530

    Fix evaluation metrics and align Member 1 implementation with frozen specification

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -37,7 +37,7 @@ class Metrics:
         accuracy = accuracy_score(y_true, y_pred)
         precision = precision_score(y_true, y_pred, average='weighted', zero_division=0)
         recall = recall_score(y_true, y_pred, average='weighted', zero_division=0)
-        f1 = f1_score(y_true, y_pred, average='weighted', zero_division=0)
+        f1 = f1_score(y_true, y_pred, average='macro', zero_division=0)
 
         return {
             'accuracy': accuracy,
